5aebca00d522f9cf901cabb00bc5593a2108682e
Romain Crestey <rcrestey@gmail.com>
2015-01-20 14:22:46 +0100

    v2 draft

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
 "use strict";
 
-/*
-    Client-side API
-    
-    Start 
-*/
+// City-core
+var cityAPIOrigin = "https://city-api.ants.builders";
+var bordeaux3dCore = require(cityAPIOrigin +'/front/index.js');
 
-module.exports = function(container, options){
-    
-    // Need a way to expose building to modify them (paint)
-    // Interaction with geoloc
-    
-    return {
-        addLight: function(light/*: THREE.Light*/){
-            throw 'TODO';
-        },
-        changeControls: function(controls){
-            throw 'TODO';
-        },
-        addMesh: function(mesh){
-            // add tramway
-            throw 'TODO';
-        }
-    };
+// City-blocks
+var SkyViewControls = require('city-blocks/controls/SkyView_RTS.js');
+var FirstPersonControls = require('city-blocks/controls/FirstPerson_PointerLock.js');
+var SunPosition = require('city-blocks/utils/SunPosition.js');
+var GeoConverter = require('city-blocks/utils/geoConverter.js');
+
+var bordeaux3DP = bordeaux3dCore(document.querySelector('#view'), cityAPIOrigin);
+
+
+function onMeshClicked(event){
+    var detail = event.detail;
+    console.log('Id', meshToBuilding.get(detail.mesh).id);
+    console.log('Intersection point', detail.point.x, detail.point.y, detail.point.z); 
+
+    ret.switchToFirstPersonView(detail.point.x, detail.point.y);
 }
+
+function onKeyPressFirstPerson(e){
+    console.log('key press while first person', e.keyCode);
+    if(e.keyCode === 27){ // escape
+        e.preventDefault();
+        ret.switchToSkyView(camera.position.x, camera.position.y);
+    }
+}
+
+var activatedControl = "Sky";
+
+var skyViewAltitude = 200;
+
+bordeaux3DP.then(function(bordeaux3D){
+
+    // initial position
+    bordeaux3D.camera.position.x = 24341.22;
+    bordeaux3D.camera.position.y = 10967.65;
+    bordeaux3D.camera.position.z = skyViewAltitude;
+
+    function toggleControls(){
+        console.log('toggleControls', activatedControl);
+
+        if(activatedControl === "Sky"){
+            bordeaux3D.changeControls(SkyViewControls, { z: skyViewAltitude });
+            //window.addEventListener('meshClicked', onMeshClicked);
+            window.removeEventListener('keydown', onKeyPressFirstPerson);
+            window.addEventListener('meshClicked', onMeshClicked);
+            
+            nextControls = "FirstPerson";
+        }
+        else{
+            skyViewAltitude = bordeaux3D.camera.position.z;
+            bordeaux3D.changeControls(FirstPersonControls);
+            window.removeEventListener('meshClicked', onMeshClicked);
+            window.addEventListener('keydown', onKeyPressFirstPerson);
+
+            nextControls = "Sky";
+        }
+    }
+    toggleControls();
+
+});
+
+
